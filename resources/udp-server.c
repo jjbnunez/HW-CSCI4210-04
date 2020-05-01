43446f7938497ce097c8fa38079416a01bb74f66
jjbnunez97 <jjbnunez97@gmail.com>
2020-05-01 19:02:10 -0400

    Add missing example source file on UDP connections

---
+++ resources/udp-server.c
@@ -0,0 +1,111 @@
+/* udp-server.c */
+
+/* To test this server, you can use the following
+   command-line netcat tool:
+
+   bash$ netcat -u linux00.cs.rpi.edu 41234
+         ^^^^^^
+      in this case, netcat will act as a client to
+       this UDP server....
+
+   The hostname (e.g., linux00.cs.rpi.edu) cannot be
+   localhost (127.0.0.1); and the port number must match what
+   the server reports.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define MAXBUFFER 8192
+
+int main()
+{
+  int sd;  /* socket descriptor -- this is actually in the fd table! */
+
+  /* create the socket (endpoint) on the server side */
+  sd = socket( AF_INET, SOCK_DGRAM, 0 );
+                    /*  ^^^^^^^^^^
+                       this will set this socket up to use UDP */
+
+  if ( sd == -1 )
+  {
+    perror( "socket() failed" );
+    return EXIT_FAILURE;
+  }
+
+  printf( "Server-side socket created on descriptor %d\n", sd );
+
+
+  struct sockaddr_in server;
+  int length;
+
+  server.sin_family = AF_INET;  /* IPv4 */
+
+  server.sin_addr.s_addr = htonl( INADDR_ANY );
+           /* any remote IP can send us a datagram */
+
+  /* specify the port number for the server */
+  server.sin_port = htons( 0 );  /* a 0 here means let the kernel assign
+                                    us a port number to listen on */
+
+  /* bind to a specific (OS-assigned) port number */
+  if ( bind( sd, (struct sockaddr *) &server, sizeof( server ) ) < 0 )
+  {
+    perror( "bind() failed" );
+    return EXIT_FAILURE;
+  }
+
+  length = sizeof( server );
+
+  /* call getsockname() to obtain the port number that was just assigned */
+  if ( getsockname( sd, (struct sockaddr *) &server, (socklen_t *) &length ) < 0 )
+  {
+    perror( "getsockname() failed" );
+    return EXIT_FAILURE;
+  }
+
+  printf( "UDP server at port number %d\n", ntohs( server.sin_port ) );
+
+
+
+
+
+  /* the code below implements the application protocol */
+  int n;
+  char buffer[ MAXBUFFER + 1 ];
+  struct sockaddr_in client;
+  int len = sizeof( client );
+
+  while ( 1 )
+  {
+    /* read a datagram from the remote client side (BLOCKING) */
+    n = recvfrom( sd, buffer, MAXBUFFER, 0, (struct sockaddr *) &client,
+                  (socklen_t *) &len );
+
+    if ( n == -1 )
+    {
+      perror( "recvfrom() failed" );
+    }
+    else
+    {
+      printf( "Rcvd datagram from %s port %d\n",
+              inet_ntoa( client.sin_addr ), ntohs( client.sin_port ) );
+
+      printf( "RCVD %d bytes\n", n );
+      buffer[n] = '\0';   /* assume that its printable char[] data */
+      printf( "RCVD: [%s]\n", buffer );
+
+      /* echo the data back to the sender/client */
+      sendto( sd, buffer, n, 0, (struct sockaddr *) &client, len );
+
+      /* to do: check the return code of sendto() */
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
