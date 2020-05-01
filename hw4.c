2415ed79d30cd9feba4736ea6aaa18b2a3b66fa1
jjbnunez97 <jjbnunez97@gmail.com>
2020-04-30 21:09:46 -0400

    Make socket object

--- hw4.c
+++ hw4.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <pthread.h>
+#include <sys/socket.h>
 
 #define PORT 9876
 
@@ -10,7 +11,15 @@ int main(int argc, char** argv)
     //Ensure no buffered output for stdout and stderr.
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
-
+    
+    //Initialize the socket object and error-check the result.
+    int socket_descriptor = socket(PF_INET, SOCK_STREAM, 0);
+    if (socket_descriptor == -1)
+    {
+        perror("MAIN: socket() call failed.\n");
+        return EXIT_FAILURE;
+    }
+    
     //Terminate.
     return 0;
 }
