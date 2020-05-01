6d83d684a463f7ec969c6656b92095b73bfe6a86
jjbnunez97 <jjbnunez97@gmail.com>
2020-04-30 20:26:43 -0400

    Disable buffered output for stdout and stderr

--- hw4.c
+++ hw4.c
@@ -2,5 +2,10 @@
 
 int main(int argc, char** argv)
 {
+    //Ensure no buffered output for stdout and stderr.
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+
+    //Terminate.
     return 0;
 }
