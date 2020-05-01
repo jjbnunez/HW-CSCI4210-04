6e28d815c28646f63963aa26ff913b93f0f3e070
jjbnunez97 <jjbnunez97@gmail.com>
2020-04-30 21:27:32 -0400

    Implement separate socket objects for TCP and UDP

--- hw4.c
+++ hw4.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <pthread.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define PORT 9876
 
@@ -12,13 +13,34 @@ int main(int argc, char** argv)
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
     
-    //Initialize the socket object and error-check the result.
-    int socket_descriptor = socket(PF_INET, SOCK_STREAM, 0);
-    if (socket_descriptor == -1)
+    //Initialize the TCP socket object, and error-check the descriptor result.
+    int tcp_descriptor = socket(PF_INET, SOCK_STREAM, 0);
+    if (tcp_descriptor == -1)
     {
-        perror("MAIN: ERROR socket() call failed.\n");
+        perror("MAIN: ERROR socket() call failed for TCP.\n");
         return EXIT_FAILURE;
     }
+
+    //Initialize the TCP socket object, and error-check the descriptor result.
+    int udp_descriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_descriptor == -1)
+    {
+        perror("MAIN: ERROR socket() call failed for UDP.\n");
+        return EXIT_FAILURE;
+    }
+
+    //Initialize a server struct for the TCP connections.
+    struct sockaddr_in tcp_server;
+    tcp_server.sin_family = PF_INET;
+    tcp_server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    //Initialize a server struct for the UDP connections.
+    struct sockaddr_in udp_server;
+    udp_server.sin_family = AF_INET;
+    udp_server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    //Specify the target port.
+    unsigned short port = 9876;
 
     //Terminate.
     return 0;
